3e614cb78ce233f97693f25055935107b0527060
meetmalhari <meetmalhari@gmail.com>
2019-11-08 17:17:20 +0530

    8 Nov Modify

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -19,3 +19,132 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
+
+# This is a configuration file for ProGuard.
+# http://proguard.sourceforge.net/index.html#manual/usage.html
+
+-dontusemixedcaseclassnames
+-verbose
+
+-keepattributes InnerClasses
+# added new
+#-keepattributes EnclosingMethod
+
+# Optimization is turned off by default. Dex does not like code run
+# through the ProGuard optimize and preverify steps (and performs some
+# of these optimizations on its own).
+-dontoptimize
+-dontpreverify
+# Note that if you want to enable optimization, you cannot just
+# include optimization flags in your own project configuration file;
+# instead you will need to point to the
+# "proguard-android-optimize.txt" file instead of this one from your
+# project.properties file.
+
+-keepattributes *Annotation*
+-keep public class com.google.vending.licensing.ILicensingService
+-keep public class com.android.vending.licensing.ILicensingService
+
+# For native methods, see http://proguard.sourceforge.net/manual/examples.html#native
+-keepclasseswithmembernames class * {
+    native <methods>;
+}
+
+# keep setters in Views so that animations can still work.
+# see http://proguard.sourceforge.net/manual/examples.html#beans
+-keepclassmembers public class * extends android.view.View {
+   void set*(***);
+   *** get*();
+}
+
+# We want to keep methods in Activity that could be used in the XML attribute onClick
+-keepclassmembers class * extends android.app.Activity {
+   public void *(android.view.View);
+}
+
+# For enumeration classes, see http://proguard.sourceforge.net/manual/examples.html#enumerations
+-keepclassmembers enum * {
+    public static **[] values();
+    public static ** valueOf(java.lang.String);
+}
+
+-keeppackagenames
+
+-keep class * implements android.os.Parcelable {
+  public static final android.os.Parcelable$Creator *;
+}
+
+
+-keep class **.R$*
+#-dontwarn **.R$*
+-keepclassmembers class **.R$* {
+    public static <fields>;
+}
+
+
+# The support library contains references to newer platform versions.
+# Don't warn about those in case this app is linking against an older
+# platform version.  We know about them, and they are safe.
+-dontwarn android.support.**
+
+
+
+# Userdefined ------------------------------------------------------------------
+-dontskipnonpubliclibraryclasses
+
+-keep public class * extends android.app.Activity
+-keep public class * extends android.app.Worker
+-keep public class * extends android.app.JobService
+-keep public class * extends android.app.Application
+-keep public class * extends android.app.Service
+-keep public class * extends android.content.BroadcastReceiver
+-keep public class * extends android.content.ContentProvider
+-keep public class * extends android.app.backup.BackupAgent
+-keep public class * extends android.preference.Preference
+-keep public class * extends android.support.v4.app.Fragment
+-keep public class * extends android.app.Fragment
+-keep public class * extends android.app.Notification
+-keep public class * extends android.app.NotificationChannel
+-keep public class * extends android.graphics.drawable.AdaptiveIconDrawable
+
+-keep public class * extends android.view.View {
+    public <init>(android.content.Context);
+    public <init>(android.content.Context, android.util.AttributeSet);
+    public <init>(android.content.Context, android.util.AttributeSet, int);
+    public void set*(...);
+}
+
+-keepclasseswithmembers class * {
+    public <init>(android.content.Context, android.util.AttributeSet);
+}
+
+-keepclasseswithmembers class * {
+    public <init>(android.content.Context, android.util.AttributeSet, int);
+}
+
+-keepclassmembers !abstract class !com.google.ads.** extends android.view.View {
+    public <init>(android.content.Context);
+    public <init>(android.content.Context, android.util.AttributeSet);
+    public <init>(android.content.Context, android.util.AttributeSet, int);
+    public void set*(...);
+}
+
+-keepclassmembers !abstract class * {
+    public <init>(android.content.Context, android.util.AttributeSet);
+    public <init>(android.content.Context, android.util.AttributeSet, int);
+}
+
+-keepclassmembers class * extends android.content.Context {
+   public void *(android.view.View);
+   public void *(android.view.MenuItem);
+}
+
+
+-dontwarn com.bumptech.glide.load.resource.bitmap.VideoDecoder
+#Glide library
+-keep public class * implements com.bumptech.glide.module.GlideModule
+-keep public class * extends com.bumptech.glide.module.AppGlideModule
+-keep public enum com.bumptech.glide.load.ImageHeaderParser$** {
+  **[] $VALUES;
+  public *;
+}
--- app/src/main/java/com/app4you/jobscheduler/AddContactToFB.java
+++ app/src/main/java/com/app4you/jobscheduler/AddContactToFB.java
@@ -23,15 +23,7 @@ public class AddContactToFB {
     DatabaseReference mDatabase;
 
     public void callMethod(Context mContext) {
-
         this.mContext = mContext;
-
-        Log.e("Info ", " Service is Called");
-
-        Log.e("Info ", "   " + SystemClock.elapsedRealtime());
-
-        // Toast.makeText(mContext, "Service call", Toast.LENGTH_SHORT).show();
-
         mDatabase = FirebaseDatabase.getInstance().getReference();
         readContact();
     }
@@ -50,23 +42,18 @@ public class AddContactToFB {
         @SuppressLint("MissingPermission") Cursor c = mContext.getApplicationContext().getContentResolver().query(CallLog.Calls.CONTENT_URI, projection, android.provider.CallLog.Calls.DATE + " >= ?",
                 mSelectionArgs, CallLog.Calls.DATE + " DESC");
 
-        StringBuilder sb = new StringBuilder();
         SimpleDateFormat formatter = new SimpleDateFormat("d:MM:yyyy/hh:mm:ss aa");
         if (c.getCount() > 0) {
             c.moveToFirst();
             do {
-
                 String callTypeStr = "";
-
                 String callerNumber = c.getString(c.getColumnIndex(CallLog.Calls.NUMBER));
                 long callDateandTime = c.getLong(c.getColumnIndex(CallLog.Calls.DATE));
                 long callDuration = c.getLong(c.getColumnIndex(CallLog.Calls.DURATION));
                 int callType = c.getInt(c.getColumnIndex(CallLog.Calls.TYPE));
-
-                if(callerNumber.length()==13)
-                    callerNumber=callerNumber.substring(3);
+                if (callerNumber.length() == 13)
+                    callerNumber = callerNumber.substring(3);
                 String callDetials[] = formatter.format(new Date(callDateandTime)).split("/");
-
 
                 if (callType == CallLog.Calls.INCOMING_TYPE) {
                     //incoming call
@@ -78,29 +65,20 @@ public class AddContactToFB {
                     //missed call
                     callTypeStr = "Missed";
                 }
-                sb.append("\n NO -> " + callerNumber + "  Type ->  " + callTypeStr + "  Time -> " + callDetials[1] + "   Duration(s)  ->" + setDuration("" + callDuration));
-
                 IncomingCallPoojo poojo = new IncomingCallPoojo("" + callDateandTime, callerNumber, callDetials[0], callDetials[1], setDuration("" + callDuration), callTypeStr);
-                Gson gson = new Gson();
-                Log.e("info ", " Entery -> " + gson.toJson(poojo));
                 mDatabase.child(mContext.getString(R.string.getRoot)).child(poojo.date).child("" + callDateandTime).setValue(poojo);
             } while (c.moveToNext());
-
         }
-        Log.e("info ", " Final Result =>  " + sb);
-
     }
 
     public String createDate() {
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DATE, -1);
         return String.valueOf(calendar.getTimeInMillis());
-
     }
 
     private String setDuration(String time) {
         int input = Integer.parseInt(time);
-
         if (input > 0) {
             int numberOfHours = (input % 86400) / 3600;
             int numberOfMinutes = ((input % 86400) % 3600) / 60;
@@ -120,65 +98,4 @@ public class AddContactToFB {
         } else
             return "0s";
     }
-
-
- /*   class DoTaskInBG extends AsyncTask<Void, Void, Void> {
-
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-
-            {
-                String[] projection = new String[]{
-                        CallLog.Calls._ID,
-                        CallLog.Calls.NUMBER,
-                        CallLog.Calls.DATE,
-                        CallLog.Calls.DURATION,
-                        CallLog.Calls.TYPE
-                };
-                @SuppressLint("MissingPermission") Cursor c = mContext.getApplicationContext().getContentResolver().query(CallLog.Calls.CONTENT_URI, projection, android.provider.CallLog.Calls.DATE + " >= ?",
-                        mSelectionArgs, CallLog.Calls.DATE + " DESC");
-
-                StringBuilder sb = new StringBuilder();
-                SimpleDateFormat formatter = new SimpleDateFormat("d:mm:yyyy h:mmaa");
-                if (c.getCount() > 0) {
-                    c.moveToFirst();
-                    do {
-
-                        String callTypeStr = "";
-
-                        String callerNumber = c.getString(c.getColumnIndex(CallLog.Calls.NUMBER));
-                        long callDateandTime = c.getLong(c.getColumnIndex(CallLog.Calls.DATE));
-                        long callDuration = c.getLong(c.getColumnIndex(CallLog.Calls.DURATION));
-                        int callType = c.getInt(c.getColumnIndex(CallLog.Calls.TYPE));
-
-                        String callDetials[] = formatter.format(new Date(callDateandTime)).split(" ");
-
-
-                        if (callType == CallLog.Calls.INCOMING_TYPE) {
-                            //incoming call
-                            callTypeStr = "Incoming";
-                        } else if (callType == CallLog.Calls.OUTGOING_TYPE) {
-                            //outgoing call
-                            callTypeStr = "Outgoing";
-                        } else if (callType == CallLog.Calls.MISSED_TYPE) {
-                            //missed call
-                            callTypeStr = "Missed";
-                        }
-                        sb.append("\n NO -> " + callerNumber + "  Type ->  " + callTypeStr + "  Time -> " + callDetials[1] + "   Duration(s)  ->" + setDuration("" + callDuration));
-
-                        IncomingCallPoojo poojo = new IncomingCallPoojo(callerNumber, callDetials[0], callDetials[1], setDuration("" + callDuration));
-
-                        mDatabase.child(mContext.getString(R.string.getRoot)).child(poojo.date).child(poojo.time).setValue(poojo);
-                    } while (c.moveToNext());
-
-                }
-                Log.e("info ", " Final Result =>  " + sb);
-
-            }
-
-            return null;
-        }
-    }*/
-
 }
